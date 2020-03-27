48ba0d0e677720c7a1a111056716b79683202ce5
DioRao89 <38475463+DioRao89@users.noreply.github.com>
2020-03-26 22:16:11 -0300

    Add files via upload

---
+++ rgp.py
@@ -0,0 +1,13 @@
+
+
+print(" eai meu consagrado, qual seu nome?")
+#recebe str - texto
+nome = input()
+
+print(" quantos anos voce tem?")
+#recebe str - texto
+idade = input()
+
+#idade = int(idade)
+print("Entao, ",nome," comecou sua aventura, no auge dos seus ",idade," anos" )
+nomeidade = nome + idade
